453829344c655a6fc0ab83db9afedb9cd2d44bca
nightfury1111111111 <linjay0208@gmail.com>
2024-07-09 20:24:19 +0530

    4

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.post(
   express.json({ type: "application/json" }),
   async (request, response) => {
     const signature = request.headers["x-hub-signature-256"];
-    const body = request.body();
+    const body = request.body;
 
     console.log({ signature });
 
